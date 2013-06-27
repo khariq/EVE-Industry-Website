934f959eafd536bd06d9ae14900e7794298aaf03
Michael Pickens <khariq@gmail.com>
2013-06-27 10:15:45 -0400

    PyDev Project created
    Misnamed directory cleaned up

--- EVEIndustryProjectManagement/manage.py
+++ EVEIndustryProjectManagement/manage.py
@@ -3,7 +3,7 @@ import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "EVEIndutryProjectManagement.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "EVEIndustryProjectManagement.settings")
 
     from django.core.management import execute_from_command_line
 
